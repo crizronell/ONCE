e3a0a3cedaa1f4c37546ec524b37abdb610b9c06
Ronell <rvillamil_180000000794@uic.edu.ph>
2023-10-28 01:00:47 +0800

    fixed bug

--- app/Components/Trailer.jsx
+++ app/Components/Trailer.jsx
@@ -5,7 +5,7 @@ function Trailer() {
     <>
       <div className="fixed w-full h-full  ">
         <iframe
-          className="absolute w-full   pointer-events-none "
+          className="absolute w-full h-full   pointer-events-none "
           width="1920"
           height="1080"
           src="https://www.youtube-nocookie.com/embed/w4cTYnOPdNk?playlist=w4cTYnOPdNk&mute=1&autoplay=1&loop=1&modestbranding=1&wmode=transparent&enablejsapi=1"
