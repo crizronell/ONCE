fe43ce0807de689d41b04e30f7c2547d0da77194
Ronell <rvillamil_180000000794@uic.edu.ph>
2023-10-28 01:16:44 +0800

    fixed iframe

--- app/Components/Trailer.jsx
+++ app/Components/Trailer.jsx
@@ -5,10 +5,10 @@ function Trailer() {
     <>
       <div className="fixed w-full h-full  ">
         <iframe
-          className="absolute w-full h-screen pointer-events-none "
+          className="absolute w-full h-full   pointer-events-none "
           width="1920"
           height="1080"
-          src="https://www.youtube-nocookie.com/embed/w4cTYnOPdNk?playlist=w4cTYnOPdNk&showinfo=0&controls=1&autohide=1&modestbranding=1&autoplay=1"
+          src="https://www.youtube-nocookie.com/embed/w4cTYnOPdNk?playlist=w4cTYnOPdNk&mute=1&autoplay=1&loop=1&modestbranding=1&wmode=transparent&enablejsapi=1&controls=0&autohide=1"
           title='TWICE "SET ME FREE" M/V Teaser 2'
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen></iframe>
