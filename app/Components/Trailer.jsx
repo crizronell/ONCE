525c5f42673bacb341a85a09f0157b17a617b977
Ronell <rvillamil_180000000794@uic.edu.ph>
2023-10-28 00:13:19 +0800

    Initial Commit

--- app/Components/Trailer.jsx
+++ app/Components/Trailer.jsx
@@ -8,7 +8,7 @@ function Trailer() {
           className="absolute w-full   pointer-events-none "
           width="1920"
           height="1080"
-          src="https://www.youtube-nocookie.com/embed/CptbHRxmpTg?playlist=CptbHRxmpTg&mute=1&autoplay=1&loop=1&modestbranding=1&wmode=transparent&enablejsapi=1"
+          src="https://www.youtube-nocookie.com/embed/w4cTYnOPdNk?playlist=w4cTYnOPdNk&mute=1&autoplay=1&loop=1&modestbranding=1&wmode=transparent&enablejsapi=1"
           title='TWICE "SET ME FREE" M/V Teaser 2'
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen></iframe>
